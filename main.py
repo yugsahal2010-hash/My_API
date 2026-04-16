c6dc2090983375ccdaee001d41bbc74307760f72
yugsahal2010-hash <yug.sahal2010@gmail.com>
2026-04-16 12:08:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class MatchInput(BaseModel):
+    current_score: int
+    wickets: int
+    overs_completed: float
+    target: int
+
+class MatchOutput(BaseModel):
+    runs_needed: int
+    balls_remaining: int
+    required_run_rate: float
+    prediction: str
+
+@app.post("/predict/win", response_model=MatchOutput)
+def predict(match: MatchInput):
+    runs_needed = match.target - match.current_score
+    
+    balls_done = (int(match.overs_completed) * 6) + (round(match.overs_completed * 10) % 10)
+    balls_remaining = 120 - balls_done
+    
+    if balls_remaining > 0:
+        rrr = (runs_needed / balls_remaining) * 6
+    else:
+        rrr = 0.0
+
+    if match.wickets >= 9 or rrr > 15:
+        prob_label = "Low Chance"
+    elif rrr > 10:
+        prob_label = "Medium Chance"
+    else:
+        prob_label = "High Chance"
+
+    return {
+        "runs_needed": runs_needed,
+        "balls_remaining": balls_remaining,
+        "required_run_rate": round(rrr, 2),
+        "prediction": prob_label
+    }
